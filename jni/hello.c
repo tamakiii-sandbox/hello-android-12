5d06c769b34ee046dac1b225f749264400c320d1
tamakiii <tamakiii@users.noreply.github.com>
2023-10-22 16:49:00 +0900

    Hello World from C!

--- jni/hello.c
+++ jni/hello.c
@@ -2,6 +2,6 @@
 #include <string.h>
 
 JNIEXPORT jstring JNICALL
-Java_com_tamakiii_tamakiii_1sandbox_hello_1android_12_MainActivity_getMessage(JNIEnv *env, jobject this) {
+Java_com_tamakiii_tamakiii_1sandbox_hello_1android_112_MainActivity_getMessage(JNIEnv *env, jobject this) {
     return (*env)->NewStringUTF(env, "Hello World from C!");
 }
