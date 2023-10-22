50356f53295c3872282b69c6c8a871c8c8b285cc
tamakiii <tamakiii@users.noreply.github.com>
2023-10-22 10:51:15 +0900

    impl copyBinaryToExecutableLocation

--- app/src/main/java/com/tamakiii/tamakiii_sandbox/hello_android_12/MainActivity.kt
+++ app/src/main/java/com/tamakiii/tamakiii_sandbox/hello_android_12/MainActivity.kt
@@ -7,21 +7,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.tamakiii.tamakiii_sandbox.hello_android_12.ui.theme.Helloandroid12Theme
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.BufferedReader
+import java.io.File
+import java.io.FileOutputStream
 import java.io.InputStreamReader
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        copyBinaryToExecutableLocation()
         setContent {
             Helloandroid12Theme {
                 // A surface container using the 'background' color from the theme
@@ -34,37 +35,47 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
 
-
-@Composable
-fun Greeting(modifier: Modifier = Modifier) {
-    val greetingText = remember { mutableStateOf("Loading...") }
-
-    LaunchedEffect(key1 = Unit) {
-        withContext(Dispatchers.IO) {
-            try {
-                val process = Runtime.getRuntime().exec("/data/local/tmp/hello")
-                val reader = BufferedReader(InputStreamReader(process.inputStream))
-                val output = reader.readLine()
-                greetingText.value = "Hello $output!"
-            } catch (e: Exception) {
-                // Handle exception by updating greetingText.value
-                greetingText.value = e.message ?: "An error occurred"
-            }
-        }
+    private fun copyBinaryToExecutableLocation() {
+        val assetManager = assets
+        val inputStream = assetManager.open("hello")  // Assume binary is named "hello"
+        val outFile = File(filesDir, "hello")
+        val outputStream = FileOutputStream(outFile)
+        inputStream.copyTo(outputStream)
+        inputStream.close()
+        outputStream.flush()
+        outputStream.close()
+        outFile.setExecutable(true)
     }
 
-    Text(
-        text = greetingText.value,
-        modifier = modifier
-    )
-}
+    @Composable
+    fun Greeting(modifier: Modifier = Modifier) {
+        var greetingText by remember { mutableStateOf("Loading...") }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Helloandroid12Theme {
-        Greeting()
+        LaunchedEffect(key1 = Unit) {
+            withContext(Dispatchers.IO) {
+                try {
+                    val process = Runtime.getRuntime().exec("${filesDir}/hello")
+                    val reader = BufferedReader(InputStreamReader(process.inputStream))
+                    val output = reader.readLine()
+                    greetingText = "# $output"
+                } catch (e: Exception) {
+                    greetingText = e.message ?: "An error occurred"
+                }
+            }
+        }
+
+        Text(
+            text = greetingText,
+            modifier = modifier
+        )
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        Helloandroid12Theme {
+            Greeting()
+        }
     }
 }
